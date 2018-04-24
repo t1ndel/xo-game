3766a09f587c06899d8ddb13b89b2ed11674e7c4
t1ndel <jorku@mail.ru>
2018-04-24 13:19:49 +0700

    Field checks have been added

--- src/xo/model/Field.java
+++ src/xo/model/Field.java
@@ -1,7 +1,7 @@
 package xo.model;
 
 import xo.model.exceptions.AlreadyOccupiedException;
-import xo.model.exceptions.InvalidePointException;
+import xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
@@ -19,16 +19,16 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) throws InvalidePointException {
+    public Figure getFigure(final Point point) throws InvalidPointException {
         if(!checkPoint(point)) {
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) throws InvalidePointException, AlreadyOccupiedException {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
         if(!checkPoint(point)) {
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
         if(field[point.x][point.y] != null) {
             throw new AlreadyOccupiedException();

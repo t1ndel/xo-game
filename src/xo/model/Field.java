f43343fcc3c9b89b1a08c8e21e7fc9b32000a40e
t1ndel <jorku@mail.ru>
2018-04-24 12:28:00 +0700

    Field - tests for the Point added

--- src/xo/model/Field.java
+++ src/xo/model/Field.java
@@ -1,22 +1,47 @@
 package xo.model;
 
+import xo.model.exceptions.AlreadyOccupiedException;
+import xo.model.exceptions.InvalidePointException;
+
 import java.awt.*;
 
 public class Field {
 
-    private final static int FIELD_SIZE = 3;
+    private static final int FIELD_SIZE = 3;
+
+    private static final int MIN_COORDINATE = 0;
+
+    private static final int MAX_COORDINATE = FIELD_SIZE;
+
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidePointException {
+        if(!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidePointException, AlreadyOccupiedException {
+        if(!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
+        if(field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 
 }

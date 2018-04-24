acd7a2b7fb652e0651efff05020b8fe9ea0fe937
t1ndel <jorku@mail.ru>
2018-04-24 19:58:48 +0700

    FieldTests have been fixed

--- tst/xo/model/FieldTest.java
+++ tst/xo/model/FieldTest.java
@@ -30,22 +30,7 @@ public class FieldTest {
 
     }
 
-    @Test
-    public void setFigureWhenAlreadyOccupied() throws Exception {
-        final Field field = new Field();
-        final Point inputPoint = new Point(0, 0);
-        final Figure inputFigure = Figure.O;
 
-        field.setFigure(inputPoint, inputFigure);
-
-        try {
-            field.setFigure(inputPoint, inputFigure);
-            fail();
-        } catch(final AlreadyOccupiedException e) {
-
-        }
-
-    }
 
     @Test
     public void getFigureWhenFigureIsNotSet() throws Exception {
